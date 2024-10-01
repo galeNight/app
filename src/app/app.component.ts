c0ba1b5a2437b1aba3f425f7edae73d79ff4644c
Jannick Mørup <jannickmorup@gmail.com>
2024-10-01 10:09:26 +0200

    ....

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +11,10 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'app';
+
+  constructor(private router: Router) {}
+  navigateToDirectors() {
+    this.router.navigate(['/directors']);
+  }
 }
+
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { DirectorsComponent } from './directors/directors.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'directors', component: DirectorsComponent},
+];
